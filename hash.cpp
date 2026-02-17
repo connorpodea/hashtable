cd0142d973639cfb8abe78e9528b3da6e0e0db4c
Connor <your_email@example.com>
2026-02-17 10:48:32 -0700

    struct and hash func

--- hash.cpp
+++ hash.cpp
@@ -4,7 +4,31 @@
 
 using namespace std;
 
-int hash_function(string text) {
+int hash_function(string text)
+{
     // Implement your own hash function here
-    
+    int sum = 0;
+    for (int i = 0; i < text.length(); i++)
+    {
+        sum += text.at(i);
+    }
+    return sum;
 }
+
+struct Node
+{
+    string data;
+    Node *next;
+
+    Node()
+    {
+        data = "";
+        next = nullptr;
+    }
+
+    Node(string input)
+    {
+        this->data = input;
+        this->next = nullptr;
+    }
+};
